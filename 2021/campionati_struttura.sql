b234b7b4225de0be6a4bfe3ca619c54ff6645a5d
Giuseppe Della Penna <giuseppe.dellapenna@univaq.it>
2022-10-20 14:27:22 +0200

    Added raw 2021 examples

---
+++ 2021/campionati_struttura.sql
@@ -0,0 +1,113 @@
+-- se stiamo effettuando delle prove, questa istruzione cancella il database ad ogni esecusione dello script
+-- in modo da ricrearlo dall'inizio e per intero. Se abbiamo già dei dati nelle tabelle evitiamolo...
+DROP DATABASE IF EXISTS campionati; 
+
+-- creaimo il database e lo selezioniamo come default per tutte le istruzioni successive
+CREATE DATABASE campionati;
+USE campionati;
+
+-- (opzionale) (ri)creiamo anche l'utente che accederà ai dati
+DROP USER IF EXISTS 'campionatiUser'@'localhost';
+CREATE USER 'campionatiUser'@'localhost' IDENTIFIED BY 'campionatiPwd';
+GRANT select,insert,update,delete,execute ON campionati.* TO 'campionatiUser'@'localhost';
+
+CREATE TABLE campionato (
+    ID INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    anno SMALLINT UNSIGNED NOT NULL,
+    CONSTRAINT campionato_distinto UNIQUE (nome , anno),
+    constraint controllo_anno CHECK(anno>1900 and anno<2500)
+);
+
+CREATE TABLE squadra (
+    ID INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    citta VARCHAR(100) NOT NULL,
+    CONSTRAINT squadra_distinta UNIQUE (nome , citta)
+);
+
+CREATE TABLE giocatore (
+    ID INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    cognome VARCHAR(50) NOT NULL,
+    luogoNascita VARCHAR(100) NOT NULL,
+    dataNascita DATE NOT NULL,
+    CONSTRAINT giocatore_distinto UNIQUE (nome , cognome , dataNascita , luogoNascita)
+);
+
+CREATE TABLE arbitro (
+    CF CHAR(16) PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    cognome VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE luogo (
+    ID INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    citta VARCHAR(100) NOT NULL,
+    CONSTRAINT luogo_distinto UNIQUE (nome , citta)
+);
+
+CREATE TABLE partita (
+    ID INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    `data` DATETIME NOT NULL,
+    ID_squadra_1 INTEGER UNSIGNED NOT NULL,
+    ID_squadra_2 INTEGER UNSIGNED NOT NULL,
+    punti_squadra_1 SMALLINT UNSIGNED DEFAULT 0,
+    punti_squadra_2 SMALLINT UNSIGNED DEFAULT 0,
+    ID_luogo INTEGER UNSIGNED NOT NULL,
+    ID_campionato INTEGER UNSIGNED NOT NULL,
+    CONSTRAINT partita_distinta UNIQUE (`data` , ID_squadra_1 , ID_squadra_2 , ID_campionato),
+    CONSTRAINT partita_squadra1 FOREIGN KEY (ID_squadra_1)
+        REFERENCES squadra (ID)
+        ON DELETE NO ACTION ON UPDATE CASCADE,
+    CONSTRAINT partita_squadra2 FOREIGN KEY (ID_squadra_2)
+        REFERENCES squadra (ID)
+        ON DELETE NO ACTION ON UPDATE CASCADE,
+    CONSTRAINT partita_luogo FOREIGN KEY (ID_luogo)
+        REFERENCES luogo (ID)
+        ON DELETE NO ACTION ON UPDATE CASCADE,
+    CONSTRAINT partita_campionato FOREIGN KEY (ID_campionato)
+        REFERENCES campionato (ID)
+        ON DELETE NO ACTION ON UPDATE CASCADE
+);
+
+CREATE TABLE formazione (
+    anno SMALLINT UNSIGNED NOT NULL,
+    numero SMALLINT UNSIGNED NOT NULL,
+    ID_squadra INTEGER UNSIGNED NOT NULL,
+    ID_giocatore INTEGER UNSIGNED NOT NULL,
+    PRIMARY KEY (anno , ID_squadra , ID_giocatore),
+    CONSTRAINT formazione_squadra FOREIGN KEY (ID_squadra)
+        REFERENCES squadra (ID)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT formazione_giocatore FOREIGN KEY (ID_giocatore)
+        REFERENCES giocatore (ID)
+        ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE segna (
+    minuto SMALLINT UNSIGNED NOT NULL,
+    ID_giocatore INTEGER UNSIGNED NOT NULL,
+    ID_partita INTEGER UNSIGNED NOT NULL,
+    tipo char(3) NULL,
+    PRIMARY KEY (minuto , ID_giocatore , ID_partita),
+    CONSTRAINT segna_giocatore FOREIGN KEY (ID_giocatore)
+        REFERENCES giocatore (ID)
+        ON DELETE NO ACTION ON UPDATE CASCADE,
+    CONSTRAINT segna_partita FOREIGN KEY (ID_partita)
+        REFERENCES partita (ID)
+        ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE direzione (
+    ID_partita INTEGER UNSIGNED NOT NULL,
+    CF_arbitro CHAR(16) NOT NULL,
+    PRIMARY KEY (ID_partita , CF_arbitro),
+    CONSTRAINT direzione_partita FOREIGN KEY (ID_partita)
+        REFERENCES partita (ID)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT direzione_arbitro FOREIGN KEY (CF_arbitro)
+        REFERENCES arbitro (CF)
+        ON DELETE NO ACTION ON UPDATE CASCADE
+);
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # SQL_Examples
- SQL examples developed during the lectures
+
+> SQL examples developed during the lectures
+
+## Usage
+
+This repository contains the SQL code developed during the lectures to exemplify database creation, population and querying.
+The code is organized in folders corresponding to academic years (i.e., 2021 means academic year 2021/2022). 
+You should only look at the files belonging to your year, since in previous editions there may be files with similar name but
+very different code, not aligned with the lectures you attended.
+
+- SQL files contain database *creation and population* code that you can directly execute in the DBMS shell.
+
+- All the *queries* developed during the lectures, as well as advanced code such as procedures and triggers,
+are included in a separate document together with examples and a brief explanation of the code.
+
+*This code will be developed and constantly updated starting from the second half of the course, so download it frequently, since it may get updated in the meanwhile.**
+
+---
+
+![University of L'Aquila](https://www.disim.univaq.it/skins/aqua/img/logo2021-2.png)
